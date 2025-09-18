5c5408b05afa4838d2d41ffef0b4980c6f394181
ga Naaa <mix9958@gmail.com>
2025-09-19 07:20:37 +0800

    9/19 temp

--- rag_tag_helper.py
+++ rag_tag_helper.py
@@ -1,4 +1,4 @@
-﻿import os
+﻿'''import os
 import json
 from langchain_ollama import OllamaLLM, OllamaEmbeddings
 from langchain_chroma import Chroma
@@ -14,8 +14,8 @@ DB_DIR = "chroma_db"
 
 # === 2. 定義檔案路徑 ===
 knowledge_base_directory = os.path.join(os.path.dirname(__file__), 'rag_knowledge_base', 'information', '醫學資訊管理師')
-input_json_path = os.path.join(knowledge_base_directory, "all_questions.json")
-output_json_path = input_json_path  # 覆寫同一份
+input_json_path = os.path.join(knowledge_base_directory, "all_questions.txt")
+output_json_path = os.path.join(knowledge_base_directory, "all_questions.json") #input_json_path  # 覆寫同一份
 
 # === 3. 建立 / 載入 Chroma 向量資料庫 ===
 def build_or_load_vectorstore():
@@ -82,3 +82,60 @@ def generate_tags_for_questions():
 
 if __name__ == "__main__":
     generate_tags_for_questions()
+
+'''
+
+from langchain_community.llms import Ollama
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+import os
+
+llm = Ollama(model="mistral")
+
+loader = TextLoader('rag_knowledge_base/information/醫學資訊管理師/all_questions.txt')
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=20,
+    chunk_overlap=5,
+    separators=[" ", ",", "\n"],
+)
+
+splited_docs = text_splitter.split_documents(loader.load())
+
+embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+vector_db = Chroma.from_documents(
+    documents=splited_docs,
+    embedding=embeddings,
+    persist_directory="db",
+    collection_name="about",
+)
+
+retriever = vector_db.as_retriever(search_kwargs={"k": 3})
+
+system_prompt = "你是一個標籤生成助手。根據提供的題目和答案，生成1到3個最相關的繁體中文標籤。標籤應該是專有名詞或短語，如果有英文專有名詞應以其中文名為主。不要有任何解釋或額外文字，不要有簡體中文字。"
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("user", "問題: {input}"),
+    ]
+)
+
+document_chain = create_stuff_documents_chain(llm, prompt_template)
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+context = []
+input_text = input("您想問什麼問題？\n>>> ")
+
+while input_text.lower() != "bye":
+    response = retrieval_chain.invoke({"input": input_text, "context": context})
+    context = response["context"]
+
+    print(response["answer"])
+
+    input_text = input(">>> ")
